8ae5b9f9a9282d59c55ab71af8ccc7b4839a41c2
Parker <1083835025@qq.com>
2025-02-06 18:36:53 +0800

    实现 分类分页查询 功能

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.entity.Category;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,4 +15,12 @@ public interface CategoryMapper {
     @Insert("insert into category (type, name, sort, status, create_time, update_time, create_user, update_user) " +
             "VALUES (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);
+
+    /**
+     * 分类分页查询
+     * @param name
+     * @param type
+     * @return
+     */
+    Page<Category> list(String name, Integer type);
 }
