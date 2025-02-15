db97d50c4791eb4dae309edc150869f5115b2253
Parker <1083835025@qq.com>
2025-02-15 10:11:54 +0800

    修改了店铺状态类名

--- sky-server/src/main/java/com/sky/controller/admin/AdminShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/AdminShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.user;
+package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -8,15 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("userShopController")
+@RestController
 @Slf4j
-@RequestMapping("/user/shop")
+@RequestMapping("/admin/shop")
 @Api(tags = "店铺相关接口")
-public class ShopController {
+public class AdminShopController {
     @Autowired
     private RedisTemplate redisTemplate;
 
     public static final String KEY = "SHOP_STATUS";
+
+    /**
+     * 店铺状态修改
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("店铺状态修改")
+    public Result setShopStatus(@PathVariable Integer status){
+        log.info("店铺状态修改为{}",status == 1 ? "营业中" : "打烊中");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
 
     /**
      * 获取店铺状态
--- sky-server/src/main/java/com/sky/controller/user/UserShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -8,28 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("adminShopController")
+@RestController
 @Slf4j
-@RequestMapping("/admin/shop")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
-public class ShopController {
+public class UserShopController {
     @Autowired
     private RedisTemplate redisTemplate;
 
     public static final String KEY = "SHOP_STATUS";
-
-    /**
-     * 店铺状态修改
-     * @param status
-     * @return
-     */
-    @PutMapping("/{status}")
-    @ApiOperation("店铺状态修改")
-    public Result setShopStatus(@PathVariable Integer status){
-        log.info("店铺状态修改为{}",status == 1 ? "营业中" : "打烊中");
-        redisTemplate.opsForValue().set(KEY, status);
-        return Result.success();
-    }
 
     /**
      * 获取店铺状态
